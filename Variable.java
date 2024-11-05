3be1ec3d96591d1cf5bf1990c57ee5262d66b9a4
Freddy Zhou <145275263+freddyzhounyc@users.noreply.github.com>
2024-11-05 18:15:58 -0500

    Variable.java

--- Variable.java
+++ Variable.java
@@ -3,11 +3,6 @@
  * is called in the code.
  *
  * @author Freddy Zhou
- *      Email: freddy.zhou@stonybrook.edu
- *      Stony Brook ID: 116580337
- *      HW #3 - C Parser
- *      CSE 214
- *      Recitation Number: R04 | TA Names: Veronica Oreshko, Anuj Sureshbhai, Alex Zuzow
  */
 
 public class Variable{
